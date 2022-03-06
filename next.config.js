f190597f9fd2076f7961454e096eb26d8f41bdcb
virusphp <packercyber@gmail.com>
2022-03-06 17:05:57 +0700

    update porto

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = nextConfig
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+    handleImages: ['jpeg'],
+    images: {
+    disableStaticImages: true
+  }
+  }],
+  
+
+  // your other plugins here
+
+]);
